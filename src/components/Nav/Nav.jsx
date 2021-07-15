9b7cdfd6428e4c8014dcad1481ccf6910b836e64
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-15 18:55:06 -0500

    Centered Navigation in Nav Bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import * as Scroll from 'react-scroll';
 import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
 import "./Nav.css";
 
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
 function Nav() {
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+
   return (
     <div className="nav">
-      <h2 className="nav-title">Mary Mettille</h2>
+      {/* <h2 className="nav-title">Mary Mettille</h2> */}
 
       <div>
         <Link className="navLink" to="/Portfolio">
