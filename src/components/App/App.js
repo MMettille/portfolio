4172b30984600d52260a5a02be52b3fb97db8fba
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-15 18:39:31 -0500

    Going with a one page app. Refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,38 @@
 import React from "react";
-import './App.css';
-import '@fontsource/roboto';
-
+import "./App.css";
+import "@fontsource/roboto";
+import { Link, animateScroll as scroll } from "react-scroll";
 // ⬇ What we need to import
-import { Route, HashRouter as Router } from "react-router-dom";
 
 // ⬇ Importing components
-import About from '../About/About'
+import About from "../About/About";
 import LandingPage from "../LandingPage/LandingPage.jsx";
-import Portfolio from '../Portfolio/Portfolio';
-import Resume from '../Resume/Resume';
+import Portfolio from "../Portfolio/Portfolio";
+import Resume from "../Resume/Resume";
 
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <LandingPage 
 
-          <Route path="/" exact>
-            <LandingPage />
-          </Route>
+      />
 
-          <Route path="/about" >
-            <About />
-          </Route>
 
-          <Route path="/resume" >
-            <Resume />
-          </Route>
 
-          <Route path="/portfolio" >
-            <Portfolio />
-          </Route>
+      <About 
 
-    </Router>
+      />
+
+
+
+      <Resume />
+
+
+
+      <Portfolio />
+
+      
+    </div>
   );
 }
 
