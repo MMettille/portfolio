c02277687f7cb42a30744184a5819a6e51f27caf
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-15 22:45:09 -0500

    Roughed in Nav Bar with My Name. Background image for landing page is not working

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,26 +1,14 @@
-import IconButton from '@material-ui/core/IconButton';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+
+import "./LandingPage.css";
 
 function LandingPage() {
-
-    return (
-        <>
-            <div className="header">
-                <IconButton>
-                    <GitHubIcon 
-                    onClick={() => window.open("https://github.com/MMettille")}
-                    />
-                </IconButton>
-                <IconButton>
-                    <LinkedInIcon 
-                    onClick={() => window.open("https://www.linkedin.com/in/mary-mettille-9220601a1/")}
-                    />
-                </IconButton>
-            </div>
-            <h1>Hullo there, I'm Mary.</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="landing">
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,76 +1,91 @@
 import React from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
+import IconButton from "@material-ui/core/IconButton";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 import "./Nav.css";
 
-
-
 function Nav() {
-
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
 
   return (
     <div className="nav">
-      <div>
+      <h3 className="nav-title">Mary Mettille</h3>
+      <div className="nav-left">
         
-        <p
-        className="navLink"
-        onClick={scrollToTop}
-        >
+        <IconButton>
+          <GitHubIcon
+            className="nav-link"
+            onClick={() => window.open("https://github.com/MMettille")}
+          />
+        </IconButton>
+        <IconButton>
+          <LinkedInIcon
+            className="nav-link"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/mary-mettille-9220601a1/"
+              )
+            }
+          />
+        </IconButton>
+      </div>
+      <div className="nav-right">
+        <p className="nav-link" onClick={scrollToTop}>
           Home
         </p>
-        <Link 
-        className="navLink"
-        activeClass="active"
-        to="portfolio"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
+        <Link
+          className="nav-link"
+          activeClass="active"
+          to="portfolio"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
         >
           Portfolio of Work
         </Link>
 
         {/* This will actually open up a pdf in another page? That page will be a pdf of my resume. Or somewhere on my page there will be
         a button to download my resume? */}
-        <Link 
-        className="navLink"
-        activeClass="active"
-        to="resume"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
+        <Link
+          className="nav-link"
+          activeClass="active"
+          to="resume"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
         >
           Resume
         </Link>
 
-        <Link 
-        className="navLink"
-        activeClass="active"
-        to="about"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
+        <Link
+          className="nav-link"
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
         >
           About
         </Link>
 
-        <Link 
-        className="navLink"
-        activeClass="active"
-        to="contact"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
+        <Link
+          className="nav-link"
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
         >
           Contact Me
         </Link>
-
       </div>
     </div>
   );
