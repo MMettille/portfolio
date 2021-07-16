6c844980b410a77e4ad3e21b67c422db4b720047
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-15 21:59:28 -0500

    LinkedIn Icon Links to my LinkedIn Profile

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 function LandingPage() {
 
@@ -9,6 +10,11 @@ function LandingPage() {
                 <IconButton>
                     <GitHubIcon 
                     onClick={() => window.open("https://github.com/MMettille")}
+                    />
+                </IconButton>
+                <IconButton>
+                    <LinkedInIcon 
+                    onClick={() => window.open("https://www.linkedin.com/in/mary-mettille-9220601a1/")}
                     />
                 </IconButton>
             </div>
