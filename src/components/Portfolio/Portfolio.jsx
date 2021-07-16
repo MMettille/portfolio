db83a46b10141022c2cc52bad497aade315f60ad
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-15 23:35:47 -0500

    Deleted most of the timeline, bringing it back to just one thing at a time

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,10 +4,11 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+
 import LocalCafeIcon from "@material-ui/icons/LocalCafe";
 import IconButton from "@material-ui/core/IconButton";
 import GitHubIcon from "@material-ui/icons/GitHub";
-
+import "./Portfolio.css";
 function Portfolio() {
   return (
     <div id="portfolio">
@@ -19,95 +20,17 @@ function Portfolio() {
           iconStyle={{ background: "rgb(33, 150, 243)", color: "#212121" }}
           icon={<LocalCafeIcon />}
         >
-          <h3 className="vertical-timeline-element-title">Group Capstone Project</h3>
+          <h3 className="vertical-timeline-element-title">
+            Group Capstone Project
+          </h3>
           <h4 className="vertical-timeline-element-subtitle">TBD</h4>
           <p id="description">
-            The capstone group project at Prime has us building an app for a 
+            The capstone group project at Prime has us building an app for a
             local nonprofit.
           </p>
-          <IconButton>
-          <GitHubIcon
-            className="nav-link"
-            onClick={() => window.open("https://github.com/MMettille")}
-          />
-        </IconButton>
+          <button>Click here to see the code</button>
+          <button>Click here to see the deployed app</button>
         </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<LocalCafeIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2008 - 2010"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<LocalCafeIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<LocalCafeIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={<LocalCafeIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Content Marketing for Web, Mobile and Social Media
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-          <p>Strategy, Social Media</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={<LocalCafeIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Agile Development Scrum Master
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>Creative Direction, User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-          icon={<LocalCafeIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Bachelor of Science in Interactive Digital Media Visual Imaging
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
-          icon={<LocalCafeIcon />}
-        />
       </VerticalTimeline>
     </div>
   );
