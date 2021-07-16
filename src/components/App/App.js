0ada9af0c814025b7b772e3300a07196b1eb133b
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-15 23:07:55 -0500

    Roughed In Portfolio Details

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,15 +10,18 @@ import LandingPage from "../LandingPage/LandingPage.jsx";
 import Portfolio from "../Portfolio/Portfolio";
 import Resume from "../Resume/Resume";
 import Nav from "../Nav/Nav";
-import Contact from "../Contact/Contact"
+import Contact from "../Contact/Contact";
 function App() {
   return (
     <div className="App">
+      <div className="wrapper">
       <LandingPage />
-      <About />
-      <Resume />
-      <Portfolio />
-      <Contact />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <div class="push"></div>
+      </div>
       <Nav />
     </div>
   );
