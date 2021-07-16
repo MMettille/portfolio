42aa3ddf83eb61cb8f23878261e35cea45a4f1d9
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-15 22:48:34 -0500

    Roughed in About Me Page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,21 @@ function About() {
   return (
     <div className="App" id="about">
       <div id="content-wrap">
-        <p>About</p>
+        <h2>Hello there!</h2>
+        <p>I once climbed one of the hardest sections of the Superior Hiking Trail. It was seven miles of strenuous, rugged, and steep terrain. 
+          While I was hiking, I considered turning around and ending the hike about 100 times (probably more, if I'm being honest!). 
+          But despite just giving birth, high humidity, and not having enough caffeine in my system, I kept going. 
+          Because I'm that person - once I set my mind to something, it happens.
+        </p>
+
+        <p>
+        Now, take that determination and put it into my day-to-day work - I may not be climbing mountains every day but I certainly do move them.
+        With the aid of my multi-colored post-it notes (you will be hard-pressed to find me without them), the most challenging and complex of projects
+        will be succinctly broken down into manageable and attainable action points. I excel in a role where I am working with teams across multiple
+        different departments; bridging the gap between the technical and non-technical world is a passion of mine.
+        </p>
+
+        <p>If you're interested in grabbing a coffee and talking code (or to get my secret recipe for trail mix), please send an email or message my way.</p>
       </div>
     </div>
   );
