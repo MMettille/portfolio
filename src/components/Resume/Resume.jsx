22d3303b928753f097f6851b974ce31d586a5958
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-17 15:32:08 -0500

    Started on Space for Resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import Button from "@material-ui/core/Button";
+import LocalCafeIcon from "@material-ui/icons/LocalCafe";
+import IconButton from "@material-ui/core/IconButton";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+import { makeStyles } from "@material-ui/core/styles";
 
 function Resume() {
   return (
     <div className="App" id="resume">
-      <div id="content-wrap">
-        <p>Resume</p>
-      </div>
+      <h1>Experience and Education</h1>
+      <h3>Click here to Download My Resume</h3>
+    <div>
+    <VerticalTimeline>
+    <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ borderTop: "7px solid rgb(75, 136, 201)" }}
+          iconStyle={{ background: "rgb(75, 136, 201)", color: "#212121" }}
+          >
+
+          </VerticalTimelineElement>
+</VerticalTimeline>
+    </div>
+      
     </div>
   );
 }
