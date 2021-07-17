6d2d5e746194cf9acf7159dded10f73db457a63f
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-17 17:22:53 -0500

    Technology Language Section - Rough in, working on images now"

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -1,8 +1,129 @@
+import "./Tech.css";
 
 function Tech() {
   return (
     <div className="App" id="tech">
-        <h1>These are the languages I work with most:</h1>
+      <h1>These are the languages I work with most:</h1>
+      <div className="skill-group">
+        <div className="skill-container">
+          <img
+            src="https://www.w3.org/html/logo/downloads/HTML5_Logo_128.png"
+            alt="HTML 5 logo"
+          />
+          <p>HTML</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/170px-CSS3_logo_and_wordmark.svg.png"
+            alt="CSS 3 logo"
+          />
+          <p>CSS</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freepnglogos.com/uploads/javascript-png/javascript-shield-logo-icon-2.png"
+            alt="Javascript logo"
+          />
+          <p>Javascript</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png"
+            alt="Purple C# logo"
+          />
+          <p>C#</p>
+        </div>
+      </div>
+      <div className="skill-group">
+        <div className="skill-container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png"
+            alt="React Atom logo"
+          />
+          <p>React</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/170px-CSS3_logo_and_wordmark.svg.png"
+            alt="CSS 3 logo"
+          />
+          <p>CSS</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freepnglogos.com/uploads/javascript-png/javascript-shield-logo-icon-2.png"
+            alt="Javascript logo"
+          />
+          <p>Javascript</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png"
+            alt="Purple C# logo"
+          />
+          <p>C#</p>
+        </div>
+      </div>
+      <div className="skill-group">
+        <div className="skill-container">
+          <img
+            src="https://www.w3.org/html/logo/downloads/HTML5_Logo_128.png"
+            alt="HTML 5 logo"
+          />
+          <p>HTML</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/170px-CSS3_logo_and_wordmark.svg.png"
+            alt="CSS 3 logo"
+          />
+          <p>CSS</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freepnglogos.com/uploads/javascript-png/javascript-shield-logo-icon-2.png"
+            alt="Javascript logo"
+          />
+          <p>Javascript</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png"
+            alt="Purple C# logo"
+          />
+          <p>C#</p>
+        </div>
+      </div>
+      <div className="skill-group">
+        <div className="skill-container">
+          <img
+            src="https://www.w3.org/html/logo/downloads/HTML5_Logo_128.png"
+            alt="HTML 5 logo"
+          />
+          <p>HTML</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/170px-CSS3_logo_and_wordmark.svg.png"
+            alt="CSS 3 logo"
+          />
+          <p>CSS</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freepnglogos.com/uploads/javascript-png/javascript-shield-logo-icon-2.png"
+            alt="Javascript logo"
+          />
+          <p>Javascript</p>
+        </div>
+        <div className="skill-container">
+          <img
+            src="https://www.freeiconspng.com/uploads/c-logo-icon-18.png"
+            alt="Purple C# logo"
+          />
+          <p>C#</p>
+        </div>
+      </div>
     </div>
   );
 }
