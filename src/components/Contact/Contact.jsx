7784c82e53d5f6a54babb425b565aebb943fecc7
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-17 23:26:46 -0500

    Added Contact Buttons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import EmailIcon from '@material-ui/icons/Email';
+import PhoneIcon from '@material-ui/icons/Phone';
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    margin: 5,
+    marginBottom: 15,
+    color: "black",
+    height: 48,
+  },
+});
 
 function Contact() {
+
+  const classes = useStyles();
+
   return (
     <div className="App" id="about">
       <div id="content-wrap">
@@ -11,28 +26,48 @@ function Contact() {
         There are several different mediums to reach me if you would like to network
         or work together! 
         </p>
-        <p>
-        Email: mary.mettille.brist@gmail.com
-        </p>
-        <p>
-        <p>
-        Phone: 651-500-0145
-        </p>
-        <p>
         <Button
-            className="header-link"
+            variant="outlined"
+             classes={{
+              root: classes.root, // class name, e.g. `root-x`
+            }}
+            onClick={() => window.open("https://github.com/MMettille")}
+            startIcon={<PhoneIcon />}
+            >
+              651-500-0145
+            </Button>
+        <Button
+            variant="outlined"
+             classes={{
+              root: classes.root, // class name, e.g. `root-x`
+            }}
+            onClick={() => window.open("https://github.com/MMettille")}
+            startIcon={<EmailIcon />}
+            >
+              mary.mettille.brist@gmail.com
+            </Button>
+            <br />
+        <Button
+            variant="outlined"
+             classes={{
+              root: classes.root, // class name, e.g. `root-x`
+            }}
             onClick={() => window.open("https://github.com/MMettille")}
             startIcon={<GitHubIcon />}
             >
               GitHub Profile
             </Button>
             <Button
-            className="header-link"
+            variant="outlined"
+            classes={{
+              root: classes.root, // class name, e.g. `root-x`
+            }}
             onClick={() => window.open("https://www.linkedin.com/in/mary-mettille-9220601a1/")}
             startIcon={<LinkedInIcon />}
             >
               LinkedIn Profile
             </Button>
+            <p>I'm looking forward to talking with you more!</p>
       </div>
     </div>
   );
